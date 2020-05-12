2c0f5826358d8506374dbbe08f4075dfbcfcf94d
gwenf <gwenfaraday@pm.me>
2020-05-12 19:00:11 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,165 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-toolbar-title>Vuetify Productivity</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <span class="hidden-sm-and-up">
+        <v-btn
+          @click.stop="drawer = !drawer"
+        >
+          Menu
+        </v-btn>
+      </span>
+
+      <!-- desktop & tablet menu -->
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn to="/" text>
+          <v-icon left>mdi-home</v-icon>
+          Home
+        </v-btn>
+        <v-btn to="store" text>
+          <v-icon left>mdi-store</v-icon>
+          Tasks
+        </v-btn>
+        <v-btn to="cart" text>
+          <v-icon left>mdi-calendar</v-icon>
+          Calendar
+        </v-btn>
+        <v-btn to="cart" text>
+          <v-icon>mdi-cart</v-icon>
+          Table
+        </v-btn>
+        <v-btn to="cart" text>
+          <v-icon>mdi-cart</v-icon>
+          Form
+        </v-btn>
+      </v-toolbar-items>
+
+      <!-- <router-link class="white--text" to="#">
+        Link 1
+      </router-link>
+      <v-divider vertical />
+      <div>
+        Link 2
+      </div> -->
+
+     <!-- <v-list
+        nav
+      >
+        <v-list-item>
+          Link 1
+        </v-list-item>
+        <v-list-item>
+          Link 2
+        </v-list-item>
+        <v-list-item>
+          Link 3
+        </v-list-item>
+     </v-list> -->
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn @click="drawer = !drawer" class="ml-4">Menu</v-btn>
+
+      <!-- <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </template> -->
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-content>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      right
+      temporary
+      absolute
+      dark
+    >
+      <v-list
+        dense
+        nav
+        class="py-0"
+      >
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/81.jpg">
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Vuetify Productivity</v-list-item-title>
+            <v-list-item-subtitle>Task Tracking</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item to="/tasks" link>
+          <v-list-item-icon>
+            <v-icon>mdi-format-list-bulleted-square</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Tasks</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/calendar" link>
+          <v-list-item-icon>
+            <v-icon>mdi-calendar</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Calendar</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/table" link>
+          <v-list-item-icon>
+            <v-icon>mdi-table</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Table</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/form" link>
+          <v-list-item-icon>
+            <v-icon>mdi-clipboard</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Form</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    drawer: false
+  })
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import Home from '../views/Home.vue'
+import Tasks from '../views/Tasks.vue'
+import Calendar from '../views/Calendar.vue'
+import Table from '../views/Table.vue'
+import Form from '../views/Form.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +16,24 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/tasks',
+    name: 'tasks',
+    component: Tasks
+  },
+  {
+    path: '/calendar',
+    name: 'calendar',
+    component: Calendar
+  },
+  {
+    path: '/Table',
+    name: 'table',
+    component: Table
+  },
+  {
+    path: '/form',
+    name: 'form',
+    component: Form
   }
 ]
 
